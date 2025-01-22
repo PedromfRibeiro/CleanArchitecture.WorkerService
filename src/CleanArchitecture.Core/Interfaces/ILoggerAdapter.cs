55b38bc6f5e9e200fc0f689fac971e25bbb70b88
Pedro Ribeiro <prs.p.ribeiro@gmail.com>
2025-01-22 12:45:42 +0000

    Adding logger adapter and implementation

---
+++ src/CleanArchitecture.Core/Interfaces/ILoggerAdapter.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanArchitecture.Core.Interfaces;
+
+// Helps if you need to confirm logging is happening
+// https://ardalis.com/testing-logging-in-aspnet-core
+public interface ILoggerAdapter<T>
+{
+    void LogInformation(string message, params object[] args);
+
+    void LogError(Exception ex, string message, params object[] args);
+}
--- src/CleanArchitecture.Worker/Program.cs
+++ src/CleanArchitecture.Worker/Program.cs
@@ -1,7 +1,11 @@
+using CleanArchitecture.Core.Interfaces;
+using CleanArchitecture.Infrastructure;
 using CleanArchitecture.Worker;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
+
+builder.Services.AddSingleton(typeof(ILoggerAdapter<>), typeof(LoggerAdapter<>));
 
 var host = builder.Build();
 host.Run();
